4b82a6df1d1dbe19651c71a9edc1f23c653ef0ab
David2508 <deividrocan2508@gmail.com>
2022-04-30 22:37:48 -0500

    add; Requerimiento 3

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -12,4 +12,11 @@ describe("Unit Tests for User class", () => {
         expect(user.dateCreated).not.toBeUndefined();
         expect(user.lastUpdated).not.toBeUndefined();
     });
+    test('Add getters', () => {
+        const user = new User (1, "david2508", "David", "Bio")
+        expect (user.getUsername).toBe("david2508")
+        expect (user.getBio).toBe("Bio")
+        expect (user.getDateCreated).not.toBeUndefined()
+        expect (user.getLastUpdated).not.toBeUndefined()
+        });
 })
