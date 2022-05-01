30be7ef77c272cdd2340273666fabacd400e4796
David2508 <deividrocan2508@gmail.com>
2022-05-01 15:06:32 -0500

    add: Requerimiento 4

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -1,7 +1,20 @@
 const User = require('./../models/user')
 class UserService {
-static create(id, username, name) {
-return new User(id, username, name, "Sin bio")
+    static create(id, username, name) {
+        return new User(id, username, name, "Sin bio")
+    }
+
+    static getInfo(user) {
+        return Object.values(user)
+    }
+    static updateUserUsername(user, name){
+        user.setUsername = name;
+    }
+    static getAllUsernames(users){
+        const usersUsernames = users.map( user => user.username)
+        return usersUsernames
+    }
+    
+
 }
-}
-module.exports= UserService
+module.exports = UserService
--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -7,4 +7,30 @@ describe("Tests for UserService", () => {
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
     });
+
+    test("2. Get all user data in a list", () => {
+        const user = UserService.create(1, "david2508", "David")
+        const userInfoInList=UserService.getInfo(user)
+        expect (userInfoInList[0]).toBe(1)
+        expect(userInfoInList[1]).toBe("david2508")
+        expect (userInfoInList[2]).toBe("David")
+        expect (userInfoInList[3]).toBe("Sin bio")
+        });
+    
+        test("3. Update username", () => {
+            const user = UserService.create(1, "david2508", "David")
+            UserService.updateUserUsername(user, "merino25")
+            expect(user.username).toBe("merino25")
+            
+            });
+        
+            test("4. Given a list of users give me the list of usernames", () => {
+                const user1=UserService.create(1, "davidmerino1", "David")
+                const user2 = UserService.create(1,"davidmerino2", "David")
+                const user3 = UserService.create(1,"davidmerino3", "David")
+                const usernames =UserService.getAllUsernames([user1, user2, user3])
+                expect (usernames).toContain("davidmerino1")
+                expect (usernames).toContain("davidmerino2")
+                expect (usernames).toContain("davidmerino3")
+                })
 })
